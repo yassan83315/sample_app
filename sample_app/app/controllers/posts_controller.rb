c6fd4688c75f070d3329bd0000f68052d210f13f
koku28.3dk@gmail.com <koku28.3dk@gmail.com>
2019-05-27 16:45:21 +0900

    fin

---
+++ sample_app/app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	# ストロングパロメーターを利用
+  	post = Post.new(post_params)
+  	# DBへ保存する
+  	post.save
+  	# 詳細画面へリダイレクト
+  	redirect_to post_path(post.id)
+  end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	post = Post.find(params[:id])
+  	post.update(post_params)
+  	redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body, :image)
+  end
+
+end
---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e0c09185cdbb24e19d1fabf350231235f2b88f756a10dd4ba47d7f240385380a96f2d6f2482743e0cec6ff83160a65dd3c032f6a067bb2984c8dcbf825c351f1'
